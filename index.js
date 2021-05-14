d46299e470397cc4dbedb1dee9b257a05b9b1576
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2021-05-14 15:44:38 +0100

    Added index.js file

---
+++ index.js
@@ -0,0 +1,7 @@
+
+let randomNumber1 = Math.random() * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+let randomImage = "dice" + randomNumber1 + ".png";
+
+let randomImageSource = "images/" + randomImage;

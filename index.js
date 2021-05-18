725680691bcdffb14ba178efbc14f09b32d34004
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2021-05-18 16:45:39 +0100

    Added Dice Javascript code

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 
-let randomNumber1 = Math.random() * 6;
-randomNumber1 = Math.floor(randomNumber1) + 1;
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 let randomImage = "dice" + randomNumber1 + ".png";
 
 let randomImageSource = "images/" + randomImage;
+
+let image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomImage2 = "dice" + randomNumber2 + ".png";
+
+let randomImageSource2 = "images/" + randomImage2;
+
+let image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+} else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+} else {
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+
+
+
+
+
+
